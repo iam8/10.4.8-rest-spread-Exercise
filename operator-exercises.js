3c268ecc76b1be29a7ed15a3b4e03e9984959605
alexm <alex-m@outlook.com>
2022-10-13 18:07:11 -0700

    Wrote code to refactor filterOutOdds() function; copied in the non-refactored code

--- operator-exercises.js
+++ operator-exercises.js
@@ -1,3 +1,17 @@
 // Ioana Alex Mititean
 // 10/13/22
 // Exercise 10.4.8: rest and spread
+
+// Given function
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+        return num % 2 === 0
+    });
+}
+
+// Refactored (ES2015) version of the filterOutOdds() function
+function refactoredFilterOutOdds(...nums) {
+
+    return nums.filter((num) => num % 2 === 0);
+}
